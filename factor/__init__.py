ea7ab8e0fc1cc723567a33330f4e4e7e1b281981
Luke Turner <github@luketurner.org>
2021-02-07 10:34:36 -0800

    Add ability to open factories from menu, abstract menu into separate module

--- factor/__init__.py
+++ factor/__init__.py
@@ -12,9 +12,7 @@ from .state import State
 
 INIT = []
 
-from .item_editor import show_item_editor
-from .factory import create_and_open_factory
-
+from .menu import add_main_menu
 
 def main():
 
@@ -36,27 +34,14 @@ def main():
     [init() for init in INIT]
 
     with window("Factory Calculator"):
-
-        with menu_bar("Main Menu Bar"):
-
-            with menu("Factory"):
-                add_menu_item("New Factory...", callback=create_and_open_factory)
-
-            with menu("World"):
-                add_menu_item("Edit items...", callback=show_item_editor)
-
-            with menu("Debug"):
-                add_menu_item("About window...", callback=show_about)
-                add_menu_item("Debug window...", callback=show_debug)
-                add_menu_item("Log window...", callback=show_logger)
-                add_menu_item("Style editor...", callback=show_style_editor)
+        add_main_menu()
 
     try:
         add_additional_font("C:\\Windows\\Fonts\\Consola.ttf", 16)
     except:
         log_error("Failed loading font")
-    set_theme("Light")
 
+    set_theme("Light")
     start_dearpygui(primary_window="Factory Calculator")
 
 
