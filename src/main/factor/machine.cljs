edf5869bc2cba0258c1214ef93195e0a1770789a
Luke Turner <github@luketurner.org>
2021-02-23 20:45:28 -0800

    new machines to start with empty name

--- src/main/factor/machine.cljs
+++ src/main/factor/machine.cljs
@@ -1,7 +1,7 @@
 (ns factor.machine
   (:require [medley.core :refer [filter-vals map-vals dissoc-in]]))
 
-(defn machine [] {:name "Unnamed machine"})
+(defn machine [] {:name ""})
 
 (defn factory-references-machine? [factory machine-id]
   (some #(contains? (% factory) machine-id) [:machines]))
