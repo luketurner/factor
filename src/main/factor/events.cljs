3447547b745559a6b00303261c260f61f1caacd4
Luke Turner <github@luketurner.org>
2021-02-28 13:25:56 -0800

    refactor events using some utility fns

--- src/main/factor/events.cljs
+++ src/main/factor/events.cljs
@@ -5,15 +5,40 @@
             [factor.machine :refer [machine]]
             [factor.item :refer [item]]
             [factor.recipe :refer [recipe-has-reference? recipe]]
-            [factor.world :refer [empty-world]]
+            [factor.world :refer [empty-world] :as world]
             [medley.core :refer [filter-vals]]))
 
 (defn reg-all-events []
-  (reg-event-db :create-factory (fn [db] (assoc-in db [:world :factories (new-uuid)] (factory))))
+  (reg-event-db :create-factory #(world/update-world % world/with-factory nil (factory)))
+  (reg-event-db :create-item #(world/update-world % world/with-item nil (item)))
+  (reg-event-db :create-recipe #(world/update-world % world/with-recipe nil (recipe)))
+  (reg-event-db :create-machine #(world/update-world % world/with-machine nil (machine)))
+
   (reg-event-db :update-factory
                 [(enrich recalc-factory)]
-                (fn [db [_ id factory]] (assoc-in db [:world :factories id] factory)))
-  (reg-event-db :delete-factory (fn [db [_ id]] (update-in db [:world :factories] dissoc id)))
+                (fn [db [_ id x]] (world/update-world db world/with-factory id x)))
+  (reg-event-db :update-item
+                (fn [db [_ id x]] (world/update-world db world/with-item id x)))
+  (reg-event-db :update-recipe
+                [(dispatch-after (fn [[_ id]] [:update-factories-with-fk :item id]))]
+                (fn [db [_ id x]] (world/update-world db world/with-recipe id x)))
+  (reg-event-db :update-machine
+                [(dispatch-after (fn [[_ id]] [:update-factories-with-fk :machine id]))
+                 (dispatch-after (fn [[_ id]] [:update-recipes-with-fk :machine id]))]
+                (fn [db [_ id x]] (world/update-world db world/with-machine id x)))
+  
+  (reg-event-db :delete-factory (fn [db [_ id]] (world/update-world db world/without-factory id)))
+  (reg-event-db :delete-recipe
+                [(dispatch-after (fn [[_ id]] [:update-factories-with-fk :recipe id]))]
+                (fn [db [_ id]] (world/update-world db world/without-recipe id)))
+  (reg-event-db :delete-item
+                [(dispatch-after (fn [[_ id]] [:update-recipes-with-fk :item id]))
+                 (dispatch-after (fn [[_ id]] [:update-factories-with-fk :item id]))]
+                (fn [db [_ id]] (world/update-world db world/without-item id)))
+  (reg-event-db :delete-machine
+                [(dispatch-after (fn [[_ id]] [:update-recipes-with-fk :machine id]))
+                 (dispatch-after (fn [[_ id]] [:update-factories-with-fk :machine id]))]
+                (fn [db [_ id]] (world/update-world db world/without-machine id)))
 
   (reg-event-fx
    :update-factories-with-fk
@@ -25,33 +50,6 @@
                      (let [factory (get-in world [:factories factory-id])]
                        [[:dispatch [:update-factory factory-id (update-foreign-keys factory type world)]]
                         [:toast (str "Updating factory " factory-id " due to " (name type) " change.")]])))})))
-
-  (reg-event-db :create-item (fn [db] (assoc-in db [:world :items (new-uuid)] (item))))
-  (reg-event-db :update-item (fn [db [_ id v]] (assoc-in db [:world :items id] v)))
-  (reg-event-db :delete-item
-                [(dispatch-after (fn [[_ id]] [:update-recipes-with-fk :item id]))
-                 (dispatch-after (fn [[_ id]] [:update-factories-with-fk :item id]))]
-                (fn [db [_ id]] (update-in db [:world :items] dissoc id)))
-  (reg-event-db :create-machine (fn [db] (assoc-in db [:world :machines (new-uuid)] (machine))))
-  (reg-event-db :update-machine
-                (fn [db [_ id v]] (assoc-in db [:world :machines id] v)))
-  (reg-event-db :delete-machine
-                [(dispatch-after (fn [[_ id]] [:update-recipes-with-fk :item id]))
-                 (dispatch-after (fn [[_ id]] [:update-factories-with-fk :item id]))]
-                (fn [db [_ id]] (update-in db [:world :machines] dissoc id)))
-
-  (reg-event-db :create-recipe
-                (fn [db [_ opt]]
-                  (let [id (new-uuid)]
-                    (cond-> db
-                      true (assoc-in [:world :recipes id] (recipe))
-                      (= opt :expanded) (assoc-in [:ui :recipes :expanded id] true)))))
-  (reg-event-db
-   :update-recipe
-   [(dispatch-after (fn [[_ id]] [:update-factories-with-fk :item id]))]
-   (fn [db [_ id v]] (assoc-in db [:world :recipes id] v)))
-  (reg-event-db :delete-recipe (fn [db [_ id]] (update-in db [:world :recipes] dissoc id)))
-  (reg-event-db :toggle-recipe-expanded (fn [db [_ id]] (update-in db [:ui :recipes :expanded id] not)))
 
   (reg-event-fx
    :update-recipes-with-fk
--- src/main/factor/world.cljs
+++ src/main/factor/world.cljs
@@ -1,7 +1,31 @@
 (ns factor.world
-  (:require [clojure.edn :as edn]))
+  (:require [clojure.edn :as edn]
+            [factor.util :refer [new-uuid]]))
 
 (def empty-world {:items {} :machines {} :recipes {} :factories {}})
 
 (defn world->str [world] (pr-str world))
 (defn str->world [s] (edn/read-string s))
+
+(defn update-world [db & rest]
+  (apply (partial update db :world) rest))
+
+(defn factories [world] (get world :factories))
+(defn machines [world] (get world :machines))
+(defn recipes [world] (get world :recipes))
+(defn items [world] (get world :items))
+
+(defn factory-ids [world] (-> world (factories) (keys)))
+(defn machine-ids [world] (-> world (machines) (keys)))
+(defn recipe-ids [world] (-> world (recipes) (keys)))
+(defn item-ids [world] (-> world (items) (keys)))
+
+(defn with-factory [world id factory] (update world :factories assoc (or id (new-uuid)) factory))
+(defn with-machine [world id machine] (update world :machines assoc (or id (new-uuid)) machine))
+(defn with-item [world id item] (update world :items assoc (or id (new-uuid)) item))
+(defn with-recipe [world id recipe] (update world :recipes assoc (or id (new-uuid)) recipe))
+
+(defn without-factory [world id] (update world :factories dissoc id))
+(defn without-machine [world id] (update world :machines dissoc id))
+(defn without-item [world id] (update world :items dissoc id))
+(defn without-recipe [world id] (update world :recipes dissoc id))
