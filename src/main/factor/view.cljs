09827402a23a7b6192a95cf4dfdacd2609b01624
Luke Turner <github@luketurner.org>
2021-06-13 11:24:28 -0700

    (WIP) play with view layer

--- src/main/factor/view.cljs
+++ src/main/factor/view.cljs
@@ -1,6 +1,6 @@
 (ns factor.view
   (:require [re-frame.core :refer [subscribe dispatch]]
-            [reagent.core :refer [adapt-react-class]]
+            [reagent.core :refer [adapt-react-class reactify-component create-class]]
             [factor.styles :as styles]
             [factor.components.factory :as factory]
             [factor.components.item :as item]
@@ -11,7 +11,73 @@
             [factor.util :refer [c]]
             ["@blueprintjs/core" :as b]
             ["@blueprintjs/select" :as bs]
-            ["ag-grid-react" :refer [AgGridColumn AgGridReact]]))
+            ["ag-grid-react" :refer [AgGridColumn AgGridReact]]
+            [medley.core :refer [map-keys]]))
+
+(defn button [props & children] (into [(c b/Button) props] children))
+
+(defn control-group [props & children]
+  (into [(c b/ControlGroup) props] children))
+
+(defn form-group [props & children]
+  (into [(c b/FormGroup) props] children))
+
+(defn suggest [type value on-change]
+  (let [values @(subscribe [(keyword (str (name type) "-names"))])]
+    [(c bs/Suggest) {:items values
+                     :selected-item value
+                     :on-item-select on-change}]))
+
+(defn numeric-input [value on-change opts]
+  [(c b/NumericInput) (merge {:value value
+                              :on-value-change on-change}
+                             opts)])
+
+(defn suggest-numeric [type [x n] on-change]
+  [control-group
+   [numeric-input n #(on-change [x %]) {:min 0}]
+   [suggest type  x #(on-change [% n])]])
+
+(defn suggest-numeric-deletable [type kvp on-change on-delete]
+  [control-group
+   [suggest-numeric type kvp on-change]
+   [button {:icon :delete :on-click on-delete}]])
+
+(defn input [{:keys [on-change] :as props}]
+  (let [override-props {:on-change #(-> % (.-target) (.-value) (on-change))}
+        props (merge props override-props)]
+    [(c b/InputGroup) props]))
+
+(defn recipe-editor [id]
+  (let [{:keys [name]} @(subscribe [:recipe id])
+        on-change #(dispatch [:update-recipe id])]
+    [:form
+     [form-group {:label "Name"}
+      [input {:value name}]]]))
+
+;; (defn collapsible [props & children]
+;;   (reagent.core/with-let [open? (reagent.core/atom false)]
+;;     [:<>
+;;      [:div  (:label props)]
+;;      (into [(c b/Collapse) {:is-open @open?}] children)]))
+
+(defn collapsible [label & children]
+  (into [:details [:summary label]] children))
+
+(defn card [p & children]
+  (into [(c b/Card) p] children))
+
+(defn recipe-editor-collapsible [id]
+  (let [{:keys [name]} @(subscribe [:recipe id])]
+    [card
+     [collapsible
+     (str "Recipe " (subs id 0 3))
+     [recipe-editor id]]]))
+
+(defn recipe-list []
+  (let [recipes @(subscribe [:recipe-ids])]
+    (into [:div {:styles {:max-width "30rem"}}] (for [r recipes] [recipe-editor-collapsible r]))))
+
 
 (defn nav-link [page icon text]
   (let [selected-page @(subscribe [:selected-page])]
@@ -50,46 +116,74 @@
    [:h2 "help"]
    [:p "Get your help here. (Once the page is finished.)"]])
 
+(defn grid-cb->clj [ev]
+  {:api (.-api ev)
+   :data (-> ev (.-data) (js->clj) (->> (map-keys keyword)))})
+
+(defn try-fn [f & args] (when (fn? f) (apply f args)))
+
+(defn grid-cb [cb]
+  (fn [ev]
+    (->> ev (grid-cb->clj) (try-fn cb))))
+
+(defn grid [props & children]
+  (let [
+        default-props  {:row-selection :multiple
+                        :enter-moves-down true
+                        :enter-moves-down-after-edit true
+                        :edit-type "fullRow"
+                        :default-col-def {:editable true}}
+        override-props {:on-row-value-changed (grid-cb (:on-row-value-changed props))
+                        :on-grid-ready        (grid-cb (:on-grid-ready        props))
+                        :on-selection-changed (grid-cb (:on-selection-changed props))}
+        grid-props     (merge default-props props override-props)]
+   [:div.ag-theme-alpine {:style {:width "100%" :height "100%"}}
+    (into [(c AgGridReact) grid-props] children)]))
+
+(defn get-selected-ids [ev]
+  (-> ev
+      (:api)
+      (.getSelectedRows)
+      (js->clj)
+      (->> (map #(get % :id)))
+      (vec)))
+
+(defn navbar [& children] (into [(c b/Navbar)] children))
+(defn navbar-heading [& children] (into [(c b/Navbar.Heading)] children))
+(defn navbar-divider [] [(c b/Navbar.Divider)])
+(defn navbar-group-left [& children] (into [(c b/Navbar.Group) {:align :left}] children))
+(defn navbar-group-right [& children] (into [(c b/Navbar.Group) {:align :right}] children))
+
+(defn selected-text [num] (str "(" num " items selected)"))
+
 (defn item-grid [items]
-  (let [update-item #(dispatch [:update-item (get % "id") {:name (get % "name")}])
+  (let [update-item #(dispatch [:update-item (get-in % [:data :id]) {:name (get-in % [:data :name])}])
         update-selection #(dispatch [:update-selection [:item %]])]
-    [:div.ag-theme-alpine {:style {:width "100%" :height "100%"}}
-     [(c AgGridReact) {:row-data items
-                       :row-selection :multiple
-                       :enter-moves-down true
-                       :enter-moves-down-after-edit true
-                       :edit-type "fullRow"
-                       :on-row-value-changed #(-> % (js->clj) (get "data") (update-item))
-                       :on-selection-changed (fn [x] (-> x
-                                                         (.-api)
-                                                         (.getSelectedRows)
-                                                         (js->clj)
-                                                         (->> (map #(get % "id")))
-                                                         (vec)
-                                                         (update-selection)))}
-      [(c AgGridColumn) {:checkboxSelection true}]
-      [(c AgGridColumn) {:field :id}]
-      [(c AgGridColumn) {:field :name
-                         :editable true
-                         :single-click-edit true}]]]))
+    [grid {:row-data items
+           :on-grid-ready #(update-selection [])
+           :on-row-value-changed update-item
+           :on-selection-changed #(-> % (get-selected-ids) (update-selection))
+           :column-defs [{:checkboxSelection true}
+                         {:field :id :editable false}
+                         {:field :name}]}]))
 
 (defn item-page-bar []
   (let [create-item #(dispatch [:create-item])
         selected-items @(subscribe [:current-selection :item])
         delete-items #(dispatch [:delete-items selected-items])
         num-selected (count selected-items)]
-    [(c b/Navbar)
-     [(c b/Navbar.Group) {:align :left}
-      [(c b/Navbar.Heading) "Item List"]
-      [(c b/Button) {:class :bp3-minimal :on-click create-item :icon :plus :title "Add item"}]
-      [(c b/Navbar.Divider)]
+    [navbar
+     [navbar-group-left
+      [navbar-heading "Item List"]
+      [button {:class :bp3-minimal :on-click create-item :icon :plus :title "Add item"}]
+      [navbar-divider]
       (when (< 0 num-selected)
         [:<>
-         [:div (str "(" num-selected " items selected)")]
-         [(c b/Button) {:class :bp3-minimal
-                        :on-click delete-items
-                        :icon :minus :text "Delete"
-                        :disabled (= num-selected 0)}]])]]))
+         [:div (selected-text num-selected)]
+         [button {:class :bp3-minimal
+                  :on-click delete-items
+                  :icon :minus :text "Delete"
+                  :disabled (= num-selected 0)}]])]]))
 
 (defn item-page [item]
   (let [all-items @(subscribe [:item-seq])]
@@ -97,17 +191,107 @@
      [item-page-bar]
      [item-grid all-items]]))
 
+(defn machine-grid [machines]
+  (let [update-machine #(dispatch [:update-machine (get-in % [:data :id]) (dissoc (:data %) :id)])
+        update-selection #(dispatch [:update-selection [:machine %]])]
+    [grid {:row-data machines
+           :on-grid-ready #(update-selection [])
+           :on-row-value-changed update-machine
+           :on-selection-changed #(-> % (get-selected-ids) (update-selection))
+           :column-defs [{:checkboxSelection true}
+                         {:field :id :editable false}
+                         {:field :name}
+                         {:field :power}
+                         {:field :speed}]}]))
+
+(defn machine-page-bar []
+  (let [create-machine #(dispatch [:create-machine])
+        selected-machines @(subscribe [:current-selection :machine])
+        delete-machines #(dispatch [:delete-machines selected-machines])
+        num-selected (count selected-machines)]
+    [navbar
+     [navbar-group-left
+      [navbar-heading "Machine List"]
+      [button {:class :bp3-minimal :on-click create-machine :icon :plus :title "Add machine"}]
+      [navbar-divider]
+      (when (< 0 num-selected)
+        [:<>
+         [:div (selected-text num-selected)]
+         [button {:class :bp3-minimal
+                  :on-click delete-machines
+                  :icon :minus :text "Delete"
+                  :disabled (= num-selected 0)}]])]]))
+
 (defn machine-page []
-  (let [machines @(subscribe [:machine-ids])]
+  (let [all-machines @(subscribe [:machine-seq])]
     [:<>
-     [:h2 "machines"]
-     [w/list-editor {:data machines
-                     :row-fn (fn [id] [machine/editor id (= id (last machines))])
-                     :empty-message [:p "You don't have any machines."]
-                     :add-fn #(dispatch [:create-machine])
-                     :del-fn #(dispatch [:delete-machine %])}]]))
+     [machine-page-bar]
+     [machine-grid all-machines]]))
+
+;; (defn rows-for-recipe [{:keys [id name input output machines]}]
+;;   (map vector (repeat id) (repeat name) input output machines))
+
+;; (defn rows-for-recipes [recipes]
+;;   (reduce (fn [rs r] (concat rs (rows-for-recipe r))) recipes))
+
+(defn recipe-io-editor [{:keys [value on-change]}]
+  [grid {:row-data value
+         :on-row-value-changed #(-> % (:data) (on-change))
+         :column-defs [{:field 0}
+                       {:field 1}]}])
+
+(defn new-recipe-cell-renderer [key]
+  (reactify-component
+   (fn [{:keys [value]}]
+     (let [data  (into [] (js->clj value))]
+     (if (not-empty data)
+       [grid {:default-col-def {:editable false}
+              :row-data data
+              :column-defs [{:field :0}
+                            {:field :1}]}])))))
+
+(defn new-recipe-cell-editor [key]
+  (reactify-component
+   (fn [{:keys [value stop-editing]}]
+     [:textarea {:on-change stop-editing} value])))
+
+(defn recipe-grid [recipes]
+  (let [update-recipe #(dispatch [:update-recipe (get-in % [:data :id]) (dissoc (:data %) :id)])
+        update-selection #(dispatch [:update-selection [:recipe %]])]
+    [grid {:row-data recipes
+           :on-grid-ready #(update-selection [])
+           :on-row-value-changed update-recipe
+           :on-selection-changed #(-> % (get-selected-ids) (update-selection))
+           :column-defs [{:checkboxSelection true}
+                         {:field :id :editable false}
+                         {:field :name}
+                         {:field :input :cellRendererFramework (new-recipe-cell-renderer :input)}]}]))
+
+(defn recipe-page-bar []
+  (let [create-recipe #(dispatch [:create-recipe])
+        selected-recipes @(subscribe [:current-selection :recipe])
+        delete-recipes #(dispatch [:delete-recipes selected-recipes])
+        num-selected (count selected-recipes)]
+    [navbar
+     [navbar-group-left
+      [navbar-heading "Recipe List"]
+      [button {:class :bp3-minimal :on-click create-recipe :icon :plus :title "Add recipe"}]
+      [navbar-divider]
+      (when (< 0 num-selected)
+        [:<>
+         [:div (str "(" num-selected " recipes selected)")]
+         [button {:class :bp3-minimal
+                        :on-click delete-recipes
+                        :icon :minus :text "Delete"
+                        :disabled (= num-selected 0)}]])]]))
 
 (defn recipe-page []
+  (let [all-recipes @(subscribe [:recipe-seq])]
+    [:<>
+     [recipe-page-bar]
+     [recipe-list]]))
+
+(defn recipe-page2 []
   (let [recipes @(subscribe [:recipe-ids])]
     [:div
      [:h2 "recipes"]
@@ -141,17 +325,6 @@
       [:dd
        [w/button {:on-click [:world-reset]} "DELETE WORLD PERMANENTLY"]]]]))
 
-;; (defn megasearch []
-;;   (let [factories @(subscribe [:factory-names])
-;;         items @(subscribe [:item-names])
-;;         machines @(subscribe [:machine-names])
-;;         recipes @(subscribe [:recipe-names])
-;;         navigate #(print "TODO" %)]
-;;     [(c bs/Suggest)
-;;      {:items (concat (keys factories) (keys items) (keys machines) (keys recipes))
-;;       :item-renderer (fn [v] (:name v))
-;;       :on-item-select navigate}]))
-
 (defn selected-page []
   (let [[x] @(subscribe [:selected-page])]
     (case x
@@ -183,6 +356,23 @@
     [(c b/AnchorButton) {:class :bp3-minimal
                          :href "https://git.sr.ht/~luketurner/factor"
                          :text "sr.ht"}]]])
+
+
+;; (defn editor-for [v cb]
+;;   (cond
+;;     (map? v) [map-editor v cb]
+;;     (seq? v) [list-editor v cb]
+;;     (string? v) [string-editor v cb]))
+
+;; (defn list-editor [v cb]
+;;   (into [] (for [i v] [editor-for i cb])))
+
+;; (defn string-editor [s cb] [:input {:type :text :value s}])
+
+;; (defn map-editor [m cb]
+;;   (into [] (for [[k v] m] [form-group {:label k} [editor-for v cb]])))
+
+
 
 (defn app []
   (let []
