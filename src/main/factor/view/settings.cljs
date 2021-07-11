433f58f34720ee71a8dc112491a5ff76d9e09c80
Luke Turner <github@luketurner.org>
2021-07-11 11:45:45 -0700

    Add world import/export to settings page

--- src/main/factor/view/settings.cljs
+++ src/main/factor/view/settings.cljs
@@ -1,11 +1,35 @@
 (ns factor.view.settings
   (:require [factor.components :as c]
             [re-frame.core :refer [dispatch subscribe]]
-            [factor.world :as w]))
+            [factor.world :as w]
+            [reagent.core :as reagent]))
 
 (defn navbar [] [c/navbar])
 
+(defn world-loader
+  []
+  (reagent/with-let [v (reagent/atom "")]
+    [:<> 
+     ;; TODO -- update textarea to extract value from on-change within component
+     ;; instead of having to do the .-target .-value jazz everywhere
+     [c/textarea {:on-change #(->> %
+                                  (.-target)
+                                  (.-value)
+                                  (reset! v))}]
+     [c/button {:text "Import"
+                :intent :warning
+                :on-click #(dispatch [:world-reset (w/str->world @v)])}]]))
+
 (defn page []
-  [:div
-   [:p "WIP"]
-   [c/button {:on-click #(dispatch [:world-reset w/empty-world]) :text "RESET WORLD"}]])
+  [:div.card-stack
+   [c/card-lg
+    [c/form-group {:label "Export (Copy World Data)"}
+     [c/textarea {:value (w/world->str @(subscribe [:world-data])) :read-only true}]]]
+   [c/card-lg
+    [c/form-group {:label "Import (Paste World Data)"}
+     [world-loader]]]
+   [c/card-lg
+    [c/form-group {:label "Delete All Data"}
+     [c/button {:on-click #(dispatch [:world-reset w/empty-world])
+                :text "RESET WORLD"
+                :intent :danger}]]]])
