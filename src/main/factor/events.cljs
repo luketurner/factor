a822654441264ac99fd4e15fcd47945fbff1e786
Luke Turner <github@luketurner.org>
2021-11-21 14:43:31 -0800

    fix: add missing factory update events

--- src/main/factor/events.cljs
+++ src/main/factor/events.cljs
@@ -31,6 +31,8 @@
                  (db/->migrate-database)]
                 (fn [db [_ w]] (assoc db :world w)))
 
+  (reg-event-db :update-factory-name [(path :world)] (fn [world [_ id name]] (assoc-in world [:factories id :name] name)))
+  (reg-event-db :update-factory-desired-output [(path :world)] (fn [world [_ id qm]] (assoc-in world [:factories id :desired-output] qm)))
 
   (reg-event-db :update-recipe-input [(path :world)] (fn [world [_ id qm]] (assoc-in world [:recipes id :input] qm)))
   (reg-event-db :update-recipe-output [(path :world)] (fn [world [_ id qm]] (assoc-in world [:recipes id :output] qm)))
