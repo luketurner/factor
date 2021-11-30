a92cd39f6fa66dc0a4685882656ba43a012f6fc6
Luke Turner <github@luketurner.org>
2021-11-29 16:23:56 -0800

    refactor: use get-effect and get-coeffect

--- src/main/factor/interceptors.cljs
+++ src/main/factor/interceptors.cljs
@@ -1,27 +1,27 @@
 (ns factor.interceptors
-  (:require [re-frame.core :refer [->interceptor get-effect assoc-effect]]
-            [malli.core :refer [validate explain]]
+  (:require [re-frame.core :refer [->interceptor get-effect get-coeffect]]
             [factor.util :refer [add-fx]]
-            [factor.schema :refer [World]]
             [day8.re-frame.undo :as undo]))
 
 (defn ->world-saver []
   (->interceptor
    :id :world-saver
-   :after (fn [{{{old-world :world} :db} :coeffects
-                {{new-world :world} :db} :effects :as context}]
-            (if (and new-world (not= new-world old-world))
-              (add-fx context [:dispatch [:world-save new-world]])
-              context))))
+   :after (fn [ctx]
+            (let [{old-world :world} (get-coeffect ctx :db)
+                  {new-world :world} (get-effect   ctx :db)]
+              (if (and new-world (not= new-world old-world))
+                (add-fx ctx [:dispatch [:world-save new-world]])
+                ctx)))))
 
 (defn ->config-saver []
   (->interceptor
    :id :config-saver
-   :after (fn [{{{old-config :config} :db} :coeffects
-                {{new-config :config} :db} :effects :as context}]
-            (if (and new-config (not= new-config old-config))
-              (add-fx context [:dispatch [:config-save new-config]])
-              context))))
+   :after (fn [ctx]
+            (let [{old-config :config} (get-coeffect ctx :db)
+                  {new-config :config} (get-effect   ctx :db)]
+              (if (and new-config (not= new-config old-config))
+                (add-fx ctx [:dispatch [:config-save new-config]])
+                ctx)))))
 
 ;; Custom interceptors that provide functions similar to [:purge-redos] event.
 (defn ->purge-undos []
