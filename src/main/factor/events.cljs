6e2dd05c3f71ade868900092a30c7bf36e7468ac
Luke Turner <github@luketurner.org>
2022-01-16 17:31:37 -0800

    fix: remove unused events

--- src/main/factor/events.cljs
+++ src/main/factor/events.cljs
@@ -188,48 +188,6 @@
   ;; World-mutating events
   ;; Undoable
 
-
-  (reg-event-db :create-factory [(undoable "New factory")
-                                 (->fragment-updater)]
-                (fn [db [_ factory]]
-                  (let [id (new-uuid)
-                        factory (->> factory
-                                     (merge {:created-at (.now js/Date) :id id})
-                                     (make Factory))]
-                    (s/multi-transform
-                     [(s/multi-path
-                       [nav/VALID-UI nav/PAGE-ROUTE (s/terminal-val [:factory id])]
-                       [nav/WORLD (nav/valid-factory id) (s/terminal-val factory)])]
-                     db))))
-
-  (reg-event-db :create-item [(undoable "New item")]
-                (fn [db [_ item]]
-                  (let [id (new-uuid)
-                        item (->> item
-                                  (merge {:created-at (.now js/Date) :id id})
-                                  (make Item))]
-                    (s/setval [nav/WORLD (nav/valid-item id)] item db))))
-
-  (reg-event-db :create-recipe [(undoable "New recipe")]
-                (fn [db [_ recipe]]
-                  (let [id (new-uuid)
-                        recipe (->> recipe
-                                    (merge {:created-at (.now js/Date) :id id})
-                                    (make Recipe))]
-                    (s/setval [nav/WORLD (nav/valid-recipe id)] recipe db))))
-
-  (reg-event-db :create-machine [(undoable "New machine")]
-                (fn [db [_ machine]]
-                  (let [id (new-uuid)
-                        machine (->> machine
-                                     (merge {:created-at (.now js/Date) :id id})
-                                     (make Machine))]
-                    (s/setval [nav/WORLD (nav/valid-machine id)] machine db))))
-
-  (reg-event-db :update-factory [(undoable "Factory changed")]
-                (fn [db [_ {:keys [id] :as x}]]
-                  (s/setval [nav/WORLD (nav/valid-factory id)] x db)))
-
   (reg-event-db :update-item    [(undoable "Item changed")]
                 (fn [db [_ {:keys [id] :as x}]]
                   (s/setval [nav/WORLD (nav/valid-item id)] x db)))
@@ -241,66 +199,6 @@
   (reg-event-db :update-recipe  [(undoable "Recipe changed")]
                 (fn [db [_ {:keys [id] :as x}]]
                   (s/setval [nav/WORLD (nav/valid-recipe id)] x db)))
-
-  (reg-event-db :delete-factories [(undoable "Delete factory")
-                                   (->fragment-updater)]
-                (fn [w [_ xs]]
-                  (let [xs (set xs)]
-                    (s/multi-transform
-                     (s/multi-path
-                      [nav/VALID-WORLD (nav/factories xs) (s/terminal-val s/NONE)]
-                      ; the below path benefit from some explaining. Basically, if
-                      ; the open factory was deleted, we want to open another,
-                      ; not-deleted factory if possible. The (collect-one) navigator
-                      ; collects the ID of another factory to open. The (s/terminal identity)
-                      ; line will then set the value of the navigated path to that collected value.
-                      ; Because multi-path transforms are run in order, the deleted
-                      ; factories have already been removed from FACTORIES-MAP when this runs.
-                      [(s/collect-one nav/WORLD nav/FACTORIES-MAP s/FIRST s/FIRST)
-                       nav/VALID-UI nav/PAGE-ROUTE (s/selected? [s/FIRST (s/pred= :factory)]) (s/nthpath 1) xs (s/terminal identity)])
-                     w))))
-
-  (reg-event-db :delete-recipes [(undoable "Delete recipes")]
-                (fn [w [_ xs]]
-                  (let [xs (set xs)]
-                    (s/setval
-                     (s/multi-path
-                      [nav/VALID-UI nav/SELECTED-OBJECTS xs]
-                      [nav/WORLD (s/multi-path
-                                  [nav/FACTORIES nav/FACTORY->RECIPES xs]
-                                  (nav/recipes xs))])
-                     s/NONE
-                     w))))
-
-  (reg-event-db :delete-machines [(undoable "Delete machines")]
-                (fn [w [_ xs]]
-                  (let [xs (set xs)]
-                    (s/setval
-                     (s/multi-path
-                      [nav/VALID-UI nav/SELECTED-OBJECTS xs]
-                      [nav/WORLD (s/multi-path
-                                  [nav/FACTORIES nav/FACTORY->MACHINES xs]
-                                  [nav/RECIPES nav/RECIPE->MACHINES xs]
-                                  (nav/machines xs))])
-                     s/NONE
-                     w))))
-
-  (reg-event-db :delete-items [(undoable "Delete items")]
-                (fn [w [_ xs]]
-                  (let [xs (set xs)]
-                    (s/setval
-                     (s/multi-path
-                      [nav/VALID-UI nav/SELECTED-OBJECTS xs]
-                      [nav/WORLD (s/multi-path
-                                  [nav/FACTORIES nav/FACTORY->ITEMS xs]
-                                  [nav/RECIPES nav/RECIPE->ITEMS xs]
-                                  (nav/items xs))])
-                     s/NONE
-                     w))))
-
-  (reg-event-db :world-reset
-                [(undoable "Reset world")]
-                (fn [db [_ w]] (assoc db :world (make World w))))
 
   (reg-event-db :load-world-from-json
                 [(undoable "Load world")]
@@ -349,7 +247,7 @@
                   (s/setval [nav/WORLD (nav/valid-recipe id) nav/DURATION] v db)))
 
 
-  ;; World persistence events
+  ;; Localstorage persistence events
   ;; Not undoable
 
   (reg-event-fx
@@ -362,14 +260,6 @@
    :world-save
    (fn [_ [_ world]]
      {:localstorage {:world (edn-encode World world)}}))
-
-  ;; Config-mutating events
-  ;; Not undoable (for now?)
-
-  (reg-event-db :set-unit (fn [db [_ k v]] (s/setval [nav/VALID-CONFIG nav/UNIT k] v db)))
-
-  ;; Config persistence events
-  ;; Not undoable
 
   (reg-event-fx
    :config-load
@@ -392,16 +282,6 @@
                 (fn [db [_ el-id]] (s/setval [nav/VALID-UI nav/FOCUSED] el-id db)))
 
   (reg-event-db :update-omnibar-query (fn [db [_ v]] (s/setval [nav/VALID-UI nav/OMNIBAR-QUERY] v db)))
-  ;; (reg-event-db :open-command-palette (fn [db] (s/multi-transform [nav/VALID-UI nav/OMNIBAR-STATE
-  ;;                                                                  (s/multi-path [nav/MODE (s/terminal-val :command-palette)]
-  ;;                                                                                [nav/QUERY (s/terminal-val "")])]
-  ;;                                                                 db)))
-  ;; (reg-event-db :omnibar-param-input (fn [db [_ param]]
-  ;;                                      (s/multi-transform [nav/VALID-UI nav/OMNIBAR-STATE
-  ;;                                                          (s/multi-path [nav/MODE (s/terminal-val :param-input)]
-  ;;                                                                        [nav/QUERY (s/terminal-val "")]
-  ;;                                                                        [nav/PARAM param])]
-  ;;                                                         db)))
   
   ;; Starts a command invocation. If the command has parameters, the omnibar is opened to prompt the user
   ;; for arguments. If there are no parameters, the command is invoked (dispatched) immediately.
